ae4c745df100f11bf9d004c56617281d05b8d519
Adam Halka <galkaadam4@gmail.com>
2024-11-20 15:02:58 +0100

    remake1

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,25 @@
 from app.main import get_coin_combination
 
 
-def test_when_zero_cents():
+def test_when_zero_cents() -> None:
     assert get_coin_combination(0) == [0, 0, 0, 0]
 
 
-def test_one_cent():
+def test_one_cent() -> None:
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
 
-def test_six_cents():
+def test_six_cents() -> None:
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
 
-def test_17_cents():
+def test_17_cents() -> None:
     assert get_coin_combination(17) == [2, 1, 1, 0]
 
 
-def test_50_cents():
+def test_50_cents() -> None:
     assert get_coin_combination(50) == [0, 0, 0, 2]
 
 
-def test_list():
+def test_list() -> None:
     assert isinstance(get_coin_combination(1), list)
